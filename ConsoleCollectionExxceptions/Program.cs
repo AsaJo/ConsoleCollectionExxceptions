f2fc573d603e9ee6ed2111e29381a35dd17969c2
Åsa Jonsson <Asa.Jonsson@lexicon.se>
2022-09-12 11:21:53 +0200

    Add project files.

---
+++ ConsoleCollectionExxceptions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleCollectionExxceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
